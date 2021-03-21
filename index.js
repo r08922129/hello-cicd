5b75944e064738d77a459eca32cea1156770c5aa
r08922129 <r08922129@csie.ntu.edu.tw>
2021-03-21 18:22:36 +0800

    dynamic port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 app.get('/',function(req,res){
   res.send('Deployed!')
 })
-var server = app.listen(8000,function(){
+var server = app.listen(process.env.PORT,function(){
 var port = server.address().port
   console.log('server is listening at port: ' + port)
 })
